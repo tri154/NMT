b15fdf7fd37de52e1ee1619e767e137dae592445
tri154 <triledacminh@gmail.com>
2025-12-08 15:34:46 +0700

    update encoder, almost done

--- src/model.py
+++ src/model.py
@@ -7,9 +7,12 @@ class Model(nn.Module):
     def __init__(self, cfg, tokenizer):
         super(Model, self).__init__()
         self.cfg = cfg
+        self.pad_id = tokenizer.pad_id
+
         self.encoder = Encoder(cfg, tokenizer)
         self.decoder = Decoder(cfg, tokenizer)
 
     def forward(self, batch_src, batch_trg, is_training):
-        print("Hello, world!")
+        src_lengths = (batch_src != self.pad_id).to(int).sum(dim=1)
+        enc_out = self.encoder(batch_src, src_lengths)
         breakpoint()
--- src/models/encoder.py
+++ src/models/encoder.py
@@ -1,17 +1,113 @@
 import torch.nn as nn
 import torch
 import torch.nn.functional as F
+import math
+
+class FeedForwardLayer(nn.Module):
+
+    def __init__(self, cfg):
+        super(FeedForwardLayer, self).__init__()
+        self.d_model = cfg.d_model
+        self.d_ff = cfg.d_ff
+        self.w1 = nn.Linear(self.d_model, self.d_ff)
+        self.w2 = nn.Linear(self.d_ff, self.d_model)
+        self.relu = nn.ReLU()
+
+    def forward(self, batch_input):
+        return self.w2(self.relu(self.w1(batch_input)))
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, cfg):
+        super(MultiHeadAttention, self).__init__()
+        assert cfg.d_model % cfg.n_heads == 0
+
+        self.device = cfg.device
+        self.n_heads = cfg.n_heads
+        self.d_model = cfg.d_model
+
+        self.wq = nn.Linear(self.d_model, self.d_model)
+        self.wk = nn.Linear(self.d_model, self.d_model)
+        self.wv = nn.Linear(self.d_model, self.d_model)
+
+        self.scale = math.sqrt(self.d_model // self.n_heads)
+
+    def forward(self, batch_input, lengths):
+        Q = self.wq(batch_input)
+        K = self.wk(batch_input)
+        V = self.wv(batch_input)
+
+        bs, mlen, _ = Q.shape
+
+        Q = Q.view(bs, mlen, self.n_heads, -1).permute(0, 2, 1, 3)
+        K = K.view(bs, mlen, self.n_heads, -1).permute(0, 2, 1, 3)
+        V = V.view(bs, mlen, self.n_heads, -1).permute(0, 2, 1, 3)
+
+        attention = torch.matmul(Q, K.permute(0, 1, 3, 2)) / self.scale
+
+        # masking there
+        # CONTINUE:
+        padding_mask = torch.arange(mlen).expand(bs, mlen) >= lengths.unsqueeze(-1)
+        padding_mask = padding_mask.unsqueeze(1).unsqueeze(2)
+
+        attn_mask = torch.zeros_like(padding_mask, dtype=torch.float32)
+        attn_mask = attn_mask.masked_fill(padding_mask, float("-inf"))
+
+
+        breakpoint()
+
+
+
+class EncoderLayer(nn.Module):
+
+    def __init__(self, cfg):
+        super(EncoderLayer, self).__init__()
+        self.ffn = FeedForwardLayer(cfg)
+        self.ffn_ln = nn.LayerNorm(cfg.d_model)
+        self.mha = MultiHeadAttention(cfg)
+        self.mha_ln = nn.LayerNorm(cfg.d_model)
+
+    def forward(self, batch_input, lengths):
+        x = self.mha(batch_input, lengths)
+        x = x + batch_input
+        x = self.mha_ln(x)
+
+        x1 = self.ffn(x)
+        x1 = x1 + x
+        x1 = self.ffn_ln(x1)
+
+        return x1
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, cfg):
+        super(PositionalEncoding, self).__init__()
+
+    def forward(self, batch_input):
+        # TODO
+        return batch_input
 
 class Encoder(nn.Module):
 
     def __init__(self, cfg, tokenizer):
         super(Encoder, self).__init__()
         self.cfg = cfg
-
+        self.n_layers = cfg.n_encoder_layers
         vocab_size = len(tokenizer.src_vocab)
 
-        self.embedding = nn.Embedding(vocab_size, self.cfg.emb_dim)
+        self.embedding = nn.Embedding(vocab_size, cfg.d_model)
+        self.pe = PositionalEncoding(cfg)
+
+        self.encoder_layers = nn.Sequential(
+            *[EncoderLayer(cfg) for _ in range(self.n_layers)]
+        )
 
     def forward(self, batch_input, lengths):
         embs = self.embedding(batch_input)
-        pass
+        embs = self.pe(embs)
+
+        for lid in range(self.n_layers):
+            embs = self.encoder_layers[lid](embs, lengths)
+
+        return embs
