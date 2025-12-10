836770e67a0e7584528c7bcf0577d7d8433f971d
tri154 <triledacminh@gmail.com>
2025-12-11 00:18:56 +0700

    beam search, almost done

--- src/tester.py
+++ src/tester.py
@@ -1,7 +1,4 @@
 import torch
-import math
-import torch.nn.functional as F
-import torch.nn.utils.rnn as rnn
 from torch.utils.data import DataLoader
 import sacrebleu
 
@@ -19,21 +16,27 @@ class Tester:
         model.eval()
         device = self.cfg.device
         eval_set = self.dev_set if tag == "dev" else self.test_set
+
         eval_collate_fn = lambda batch: eval_set.collate_fn(batch, for_training=False)
-        eval_dataloader = DataLoader(eval_set,
-                                     collate_fn=eval_collate_fn,
-                                     batch_size=batch_size,
-                                     shuffle=False,
-                                     drop_last=False)
+
+        eval_dataloader = DataLoader(
+            eval_set,
+            collate_fn=eval_collate_fn,
+            batch_size=batch_size,
+            shuffle=False,
+            drop_last=False
+        )
 
         preds = []
         labels = []
         for idx_batch, (batch_src, batch_trg) in enumerate(eval_dataloader):
             batch_src = batch_src.to(device)
             with torch.no_grad():
-                batch_preds = model(batch_src, None, is_training=False)
+                batch_preds = model(batch_src)
                 preds.extend(batch_preds)
                 labels.extend(batch_trg)
+                # break
         preds = tokenizer.detokenize(preds)
+        # breakpoint()
         score = self.cal_score(preds, labels)
         return score
