91fdd47997db02227eb672a814be11937abd17ad
tri154 <triledacminh@gmail.com>
2025-12-14 22:15:18 +0700

    custom loss

--- common/loss.py
+++ common/loss.py
@@ -13,8 +13,8 @@ class Loss:
         self.pad_id = tokenizer.pad_id
 
     def compute_loss(self, logits, labels):
-        # return self.label_smoothing(logits, labels)
-        return self.compute_cross_entropy(logits, labels)
+        return self.label_smoothing(logits, labels)
+        # return self.compute_cross_entropy(logits, labels)
 
     def compute_cross_entropy(self, logits, labels):
         # tackle padding.
@@ -25,23 +25,6 @@ class Loss:
         )
 
         return loss
-
-    def label_smoothing_old(self, pred, target):
-        bs, mlen, vocab_size = pred.shape
-        pred = pred.view(-1, vocab_size)
-        target = target.view(-1)
-        pred = pred.log_softmax(dim=-1)
-        with torch.no_grad():
-            true_dist = torch.zeros_like(pred)
-            true_dist.fill_(self.cfg.label_smoothing / (self.vocab_size - 2))
-            true_dist.scatter_(1, target.unsqueeze(1), self.confidence)
-            true_dist[:, self.pad_id] = 0
-            mask = torch.nonzero(target == self.pad_id)
-            if mask.dim() > 0:
-                true_dist.index_fill_(0, mask.squeeze(), 0.0)
-
-        return torch.mean(torch.sum(-true_dist * pred, dim=-1))
-
 
     def label_smoothing(self, pred, target):
         bs, mlen, vocab_size = pred.shape
