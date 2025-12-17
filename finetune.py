b02c077e2e803f1514f0be2195d224d626f3896a
tri154 <triledacminh@gmail.com>
2025-12-17 18:35:36 +0700

    update finetune

--- finetune.py
+++ finetune.py
@@ -157,7 +157,7 @@ if __name__ == "__main__":
         output_dir = OUTPUT_DIR,
         per_device_train_batch_size = 32,
         per_device_eval_batch_size = 32,
-        gradient_accumulation_steps = 4,
+        gradient_accumulation_steps = 1,
 
         learning_rate = 1e-4,
         num_train_epochs = 1,
